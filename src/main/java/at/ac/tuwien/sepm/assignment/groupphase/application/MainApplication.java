9ac0b3c74ad1996372b59670a2177c6d331173af
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2018-05-01 23:46:16 +0200

    Refs #25128 - Designed an icon for our tool and updated the project description

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/MainApplication.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/MainApplication.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepm.assignment.groupphase.util.SpringFXMLLoader;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
@@ -26,6 +27,7 @@ public final class MainApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
         // setup application
         primaryStage.setTitle("[Lerntia] Lern- und Prüfungstool");
+        primaryStage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/icons/main.png")));
         primaryStage.setMaximized(true);
         primaryStage.centerOnScreen();
         primaryStage.setOnCloseRequest(event -> LOG.debug("Application shutdown initiated"));
