6af1134a3e4e61fe938ae599f0ae19b82c4acf69
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2018-05-04 21:52:26 +0200

    Refs #25656 - Adapted the alignment of elements to let it adjust itself to the current window size

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/LerntiaController.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/LerntiaController.java
@@ -1,8 +1,9 @@
 package at.ac.tuwien.sepm.assignment.groupphase.lerntia.ui;
-import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceException;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.LerntiaService;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,10 +15,23 @@ public class LerntiaController {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private final LerntiaService lerntiaService;
+    @FXML
+    private HBox mainWindow;
+    @FXML
+    private VBox mainWindowLeft;
+    @FXML
+    private VBox mainWindowRight;
 
 
     public LerntiaController(LerntiaService lerntiaService) {
         this.lerntiaService = lerntiaService;
+    }
+
+    @FXML
+    private void initialize() {
+        mainWindowLeft.prefWidthProperty().bind(mainWindow.widthProperty().divide(100).multiply(25));
+        mainWindowRight.prefWidthProperty().bind(mainWindow.widthProperty().divide(100).multiply(75));
+
     }
 
     @FXML
