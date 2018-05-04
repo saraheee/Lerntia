10c562080de7b4436a0eba992f66499ca72492ac
S.Huemer <huemer.stefan@gmx.net>
2018-05-04 17:08:03 +0200

    Refs #25146 - added DAO interface and class for file import

---
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/dao/IQuestionaireImportDAO.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao;
+
+public interface IQuestionaireImportDAO {
+
+    public String getContents( String filePath );
+
+}
---
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/dao/QuestionaireImportDAO.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao;
+
+public class QuestionaireImportDAO implements IQuestionaireImportDAO {
+
+    @Override
+    public String getContents(String filePath) {
+        return null;
+    }
+
+}
