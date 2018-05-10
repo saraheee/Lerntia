b8217533bc52480c4e1f9526ee16d7edb5bc6b32
Andrea Julia Kracker <krackera@gmx.net>
2018-05-10 17:32:58 +0200

    add alerts Refs #25111

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/ImportFileController.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/ImportFileController.java
@@ -12,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -21,13 +22,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import java.io.File;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
-
-import static org.springframework.util.Assert.notNull;
 
 @Controller
 public class ImportFileController {
@@ -59,8 +57,6 @@ public class ImportFileController {
 
     @Autowired
     public ImportFileController(SimpleCourseService cservice, SimpleQuestionnaireImportService qservice) {
-        //notNull(cservice, "'SimpleCourseService' should not be null");
-        //notNull(qservice, "'SimpleQuestionnaireImportService' should not be null");
         this.cservice = cservice;
         this.qservice = qservice;
     }
@@ -68,7 +64,6 @@ public class ImportFileController {
     @FXML
     private void initialize() throws ServiceException {
         coursedata = cservice.readAll();
-        System.out.println("----------------"+coursedata.size());
         ObservableList<Course> courses = FXCollections.observableArrayList(coursedata);
         for (int i = 0; i < courses.size(); i++) {
             choices.add(courses.get(i).getMark());
@@ -84,14 +79,31 @@ public class ImportFileController {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV", "*.csv"));
         Stage stage = new Stage();
         file = fileChooser.showOpenDialog(stage);
+        t_filename.setText(file.getName());
     }
 
     @FXML
     public void importFile(ActionEvent actionEvent) {
-        /*
         if (file != null) {
             try {
-                qservice.importQuestionnaire(file);
+                String name = tf_questionnaire.getText();
+                if (!name.equals("")) {
+                    qservice.importQuestionnaire(file);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("[Lerntia] Import erfolgreich");
+                    alert.setHeaderText("Erfolgreich");
+                    alert.setContentText("Alle Fragen wurden erfolgreich importiert");
+                    alert.setResizable(true);
+                    alert.showAndWait();
+                }
+                else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("[Lerntia] Fehlerhafter Name");
+                    alert.setHeaderText("Warnung");
+                    alert.setContentText("Bitte gib einen gültigen Namen an!");
+                    alert.setResizable(true);
+                    alert.showAndWait();
+                }
             }
             catch (ServiceException e) {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -101,13 +113,14 @@ public class ImportFileController {
                 alert.setResizable(true);
                 alert.showAndWait();
             }
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("[Lerntia] Import erfolgreich");
-            alert.setHeaderText("Erfolgreich");
-            alert.setContentText("Alle Fragen wurden erfolgreich importiert");
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("[Lerntia] Kein File ausgewählt");
+            alert.setHeaderText("Achtung");
+            alert.setContentText("Bitte wähle zuerst eine csv-Datei aus!");
             alert.setResizable(true);
             alert.showAndWait();
         }
-        */
     }
 }
