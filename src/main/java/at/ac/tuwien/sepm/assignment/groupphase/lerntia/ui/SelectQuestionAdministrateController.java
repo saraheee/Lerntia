2fadd7c80424ad601a3a57c05ce14a08baf4827d
Magdy <magdy.elsadany@hotmail.com>
2018-05-19 14:39:08 +0200

    Refs #26622 - Feature is almost Done. (Missing: Question Number should be replaced)

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/SelectQuestionAdministrateController.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/ui/SelectQuestionAdministrateController.java
@@ -1,6 +1,5 @@
 package at.ac.tuwien.sepm.assignment.groupphase.lerntia.ui;
 
-import at.ac.tuwien.sepm.assignment.groupphase.exception.ControllerException;
 import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceException;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.Question;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.IMainLerntiaService;
@@ -10,12 +9,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
@@ -31,13 +28,11 @@ public class SelectQuestionAdministrateController {
     @FXML public TableView<Question> tv_questionTable;
     @FXML public TableColumn<Question, Long> tc_id;
     @FXML public TableColumn<Question, String> tc_question;
-    @FXML public TableView<Question> tv_details;
     @FXML public TableColumn<Question, String> tc_answer1;
     @FXML public TableColumn<Question, String> tc_answer2;
     @FXML public TableColumn<Question, String> tc_answer3;
     @FXML public TableColumn<Question, String> tc_answer4;
     @FXML public TableColumn<Question, String> tc_answer5;
-    @FXML public TableColumn<Question, Long> tc_detailsID;
 
     private final LerntiaMainController lerntiaMainController;
     private final IMainLerntiaService lerntiaService;
@@ -62,7 +57,6 @@ public class SelectQuestionAdministrateController {
         }
         //Fill the First Table.
         tc_id.setCellValueFactory(new PropertyValueFactory<Question, Long>("id"));
-        tc_detailsID.setCellValueFactory(new PropertyValueFactory<Question, Long>("id"));
         tc_question.setCellValueFactory(new PropertyValueFactory<Question, String>("questionText"));
         tc_answer1.setCellValueFactory(new PropertyValueFactory<Question, String>("answer1"));
         tc_answer2.setCellValueFactory(new PropertyValueFactory<Question, String>("answer2"));
@@ -71,10 +65,7 @@ public class SelectQuestionAdministrateController {
         tc_answer5.setCellValueFactory(new PropertyValueFactory<Question, String>("answer5"));
         ObservableList<Question> content = this.getContent();
         tv_questionTable.getItems().addAll(content);
-
-        //Allow the First table more the one Selection and close the Selection for the secound one.
         tv_questionTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        tv_details.getSelectionModel().setSelectionMode(null);
     }
     /**
      * Loads the Data into the TableView
@@ -89,26 +80,6 @@ public class SelectQuestionAdministrateController {
             content.add(lerntiaService.getQuestionList().get(i));
         }
         return content;
-    }
-
-    @FXML
-    public void selectQuestion(ActionEvent actionEvent) {
-        ObservableList<Question> selectedItems = tv_questionTable.getSelectionModel().getSelectedItems();
-        if(selectedItems.size() == 0){
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Fehler!");
-            alert.setHeaderText("Bitte min. 1 Frage auswählen.");
-            alert.setContentText(null);
-            alert.showAndWait();
-        }
-
-        //Delete the Current content of the Table
-        for(int i = 0;i<tv_details.getItems().size();i++){
-            tv_details.getItems().clear();
-        }
-
-        //Load the new content intu the Table
-        tv_details.getItems().addAll(selectedItems);
     }
 
     /**
@@ -131,11 +102,6 @@ public class SelectQuestionAdministrateController {
         } catch (IOException e) {
             LOG.error("Failed to open a window for administrating a questionnaire. " + e.getMessage());
         }
-    }
-
-    @FXML
-    public void backButton(ActionEvent actionEvent) {
-        //Todo Close the Current window and Open the Last one.
     }
 
     @FXML
