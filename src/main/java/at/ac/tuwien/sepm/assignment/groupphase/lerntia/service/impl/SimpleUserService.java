9ac23cd24a1773d950f31599401b8216945de2ca
Jovan <jovanmail2@gmail.com>
2018-06-10 16:57:03 +0200

    Show Data in the Window before starting an exam.
    Data is read by SimpleUserService and passed to SelectExamController

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/service/impl/SimpleUserService.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/service/impl/SimpleUserService.java
@@ -4,6 +4,7 @@ import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceException;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao.impl.UserDAO;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.User;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.IUserService;
+import at.ac.tuwien.sepm.assignment.groupphase.util.ConfigReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -13,31 +14,25 @@ import java.lang.invoke.MethodHandles;
 @Service
 public class SimpleUserService implements IUserService {
 
-    private final UserDAO userDAO;
+    private final ConfigReader configReader;
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
 
     public SimpleUserService(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
-
-    @Override
-    public void create(User user) throws ServiceException {
-
+        this.configReader = new ConfigReader("student");
     }
 
     @Override
     public void update(User user) throws ServiceException {
-
+        // todo change config file
     }
 
     @Override
-    public User read(User user) throws ServiceException {
-        return null;
-    }
-
-    @Override
-    public void delete(User user) throws ServiceException {
-
+    public User read() throws ServiceException {
+        try {
+            return new User(configReader.getValue("name"), configReader.getValue("matriculationNumber"), configReader.getValue("studyProgramme"), false);
+        } catch (Exception e) {
+            throw new ServiceException(e.getMessage());
+        }
     }
 }
