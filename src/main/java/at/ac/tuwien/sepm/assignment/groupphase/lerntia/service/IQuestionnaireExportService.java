52e33073a8b9dea8b1eca28ebed168cce4c55393
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2018-06-12 01:40:14 +0200

    Refs #28450 - Used file only in the dao layer for the csv export, updated tts to use standard values if no config file is provided, and updated ui to use the already defined css

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/service/IQuestionnaireExportService.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/service/IQuestionnaireExportService.java
@@ -1,9 +1,9 @@
 package at.ac.tuwien.sepm.assignment.groupphase.lerntia.service;
 
 import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceException;
+import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceValidationException;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.LearningQuestionnaire;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.Question;
-import org.apache.commons.io.FileExistsException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,10 +15,10 @@ public interface IQuestionnaireExportService {
      * Exports the selected questionnaire as a CSV file. After that, it selects the StudyMode again as a default.
      *
      * @param questionnaire the questionnaire to export, which name gets used for the filename
-     * @throws ServiceException    if exporting the questionnaire fails
-     * @throws FileExistsException if the file already exists
+     * @throws ServiceException           if exporting the questionnaire fails
+     * @throws ServiceValidationException if the validation fails and the file already exists
      */
-    void exportSelectedQuestionnaire(LearningQuestionnaire questionnaire) throws ServiceException, FileExistsException;
+    void exportSelectedQuestionnaire(LearningQuestionnaire questionnaire) throws ServiceException, ServiceValidationException;
 
     /**
      * Gets every question for the selected questionnaire
@@ -32,8 +32,8 @@ public interface IQuestionnaireExportService {
      * Overwrites an existing CSV file with the current version of the questionnaire
      *
      * @param questionnaire the questionnaire to export
-     * @throws ServiceException    if exporting the questionnaire fails
-     * @throws FileExistsException if the file already exists
+     * @throws ServiceException           if exporting the questionnaire fails
+     * @throws ServiceValidationException if the validation fails and the file already exists
      */
-    void overwriteFile(LearningQuestionnaire questionnaire) throws ServiceException, FileExistsException;
+    void overwriteFile(LearningQuestionnaire questionnaire) throws ServiceException, ServiceValidationException;
 }
