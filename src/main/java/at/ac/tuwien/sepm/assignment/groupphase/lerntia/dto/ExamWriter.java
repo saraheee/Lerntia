7d05debd2eb2ab8811e08d2de324a46135df03dc
S.Huemer <huemer.stefan@gmx.net>
2018-06-17 18:44:41 +0200

    Refs #28752 - finished object useage for examwriter

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/dto/ExamWriter.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/dto/ExamWriter.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class ExamWriter {
 
     private List<Question> questions;
+    private User student;
     private String name;
     private String path;
 
-    public ExamWriter(List<Question> questions, String name, String path) {
+    public ExamWriter(List<Question> questions, User student, String name, String path) {
         this.questions = questions;
+        this.student = student;
         this.name = name;
         this.path = path;
     }
@@ -36,5 +38,13 @@ public class ExamWriter {
 
     public void setPath(String path) {
         this.path = path;
+    }
+
+    public User getUser() {
+        return student;
+    }
+
+    public void setUser(User student) {
+        this.student = student;
     }
 }
