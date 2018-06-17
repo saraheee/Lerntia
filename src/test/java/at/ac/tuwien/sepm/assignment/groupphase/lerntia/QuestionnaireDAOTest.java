66ad09135caa64b7bb0217efc9bf52c4843b651d
Andrea Julia Kracker <krackera@gmx.net>
2018-06-17 18:16:33 +0200

    add more tests of QuestionnaireDAO Refs #27904

--- src/test/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/QuestionnaireDAOTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/QuestionnaireDAOTest.java
@@ -7,6 +7,7 @@ import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao.impl.CourseDAO;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao.impl.QuestionnaireDAO;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.Course;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.LearningQuestionnaire;
+import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.Questionnaire;
 import at.ac.tuwien.sepm.assignment.groupphase.util.JDBCConnectionManager;
 import at.ac.tuwien.sepm.assignment.groupphase.util.Semester;
 import org.junit.After;
@@ -19,6 +20,8 @@ import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import static junit.framework.TestCase.assertTrue;
 
 
 public class QuestionnaireDAOTest {
@@ -87,6 +90,46 @@ public class QuestionnaireDAOTest {
         LearningQuestionnaire chapter1 = new LearningQuestionnaire();
         chapter1.setName("Chapter 1");
         questionnaireDAO.create(chapter1);
+    }
+
+    @Test
+    public void checkNameOfQuestionnaire() throws PersistenceException {
+        Course tgi = new Course();
+        tgi.setSemester(Semester.SS + "2018");
+        tgi.setMark("999.349");
+        tgi.setName("TGI");
+        courseDAO.create(tgi);
+
+        LearningQuestionnaire chapter1 = new LearningQuestionnaire();
+        chapter1.setName("Chapter 1");
+        chapter1.setCourseID(tgi.getId());
+        questionnaireDAO.create(chapter1);
+
+        Long id = chapter1.getId();
+        String name = questionnaireDAO.getQuestionnaireName(id);
+        assertTrue(name.equals("Chapter 1"));
+    }
+
+    @Test
+    public void checkSelectAndDeselect() throws PersistenceException {
+        Course tgi = new Course();
+        tgi.setSemester(Semester.SS + "2018");
+        tgi.setMark("999.349");
+        tgi.setName("TGI");
+        courseDAO.create(tgi);
+
+        LearningQuestionnaire chapter1 = new LearningQuestionnaire();
+        chapter1.setName("Chapter 1");
+        chapter1.setCourseID(tgi.getId());
+        questionnaireDAO.create(chapter1);
+
+        questionnaireDAO.select(chapter1);
+        Questionnaire q = questionnaireDAO.getSelected();
+        assertTrue(q.getName().equals("Chapter 1"));
+
+        questionnaireDAO.deselect(chapter1);
+        q = questionnaireDAO.getSelected();
+        assertTrue(q == null);
     }
 
 }
