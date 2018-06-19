b01c16f53d0a4fe3fc313dd3a9f213916833e7f6
Fabio <e1526912@student.tuwien.ac.at>
2018-06-19 12:03:14 +0200

    Created constructor for the mainlerntiaservice Test (was quite tricky)

--- src/test/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/IMainLerntiaServiceTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/groupphase/lerntia/IMainLerntiaServiceTest.java
@@ -3,16 +3,10 @@ package at.ac.tuwien.sepm.assignment.groupphase.lerntia;
 import at.ac.tuwien.sepm.assignment.groupphase.exception.PersistenceException;
 import at.ac.tuwien.sepm.assignment.groupphase.exception.ServiceException;
 import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dao.impl.*;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.ILearnAlgorithmService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.ILearningQuestionnaireService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.IQuestionService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.IQuestionnaireQuestionService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.impl.LearnAlgorithmService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.impl.SimpleLearningQuestionnaireService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.impl.SimpleQuestionService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.impl.SimpleQuestionnaireQuestionService;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.ui.AlertController;
-import at.ac.tuwien.sepm.assignment.groupphase.lerntia.ui.LearnAlgorithmController;
+import at.ac.tuwien.sepm.assignment.groupphase.lerntia.dto.Question;
+import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.*;
+import at.ac.tuwien.sepm.assignment.groupphase.lerntia.service.impl.*;
+import at.ac.tuwien.sepm.assignment.groupphase.lerntia.ui.*;
 import at.ac.tuwien.sepm.assignment.groupphase.util.JDBCConnectionManager;
 import org.junit.After;
 import org.junit.Assert;
@@ -24,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import java.lang.invoke.MethodHandles;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import static org.hamcrest.core.Is.is;
 
@@ -31,7 +26,7 @@ public class IMainLerntiaServiceTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private Connection connection;
-
+    private LerntiaMainController lerntiaMainController;
     private JDBCConnectionManager jdbcConnectionManager = new JDBCConnectionManager();
     private QuestionnaireDAO questionnaireDAO;
     private ILearningQuestionnaireService learningQuestionnaireService;
@@ -40,6 +35,7 @@ public class IMainLerntiaServiceTest {
     private ILearnAlgorithmService learnAlgorithmService;
     private LearnAlgorithmController learnAlgorithmController;
     private AlertController alertController;
+    private IMainLerntiaService mainLerntiaService;
 
     @After
     public void rollback() throws SQLException {
@@ -54,35 +50,46 @@ public class IMainLerntiaServiceTest {
             JDBCConnectionManager.setIsTestConnection(true);
             connection = jdbcConnectionManager.getTestConnection();
             this.IQuestionnaireDAO(new QuestionnaireDAO(jdbcConnectionManager));
-            this.IMainLerntiaService(new SimpleLearningQuestionnaireService(new LearningQuestionnaireDAO(questionnaireDAO, jdbcConnectionManager)),
-                new SimpleQuestionService(new QuestionDAO(jdbcConnectionManager, new LearnAlgorithmDAO(jdbcConnectionManager))),
+            this.IMainLerntiaService(new MainLerntiaService(new SimpleLearningQuestionnaireService(new LearningQuestionnaireDAO(new QuestionnaireDAO(jdbcConnectionManager),jdbcConnectionManager)),
+                new SimpleQuestionService(new QuestionDAO(jdbcConnectionManager,new LearnAlgorithmDAO(jdbcConnectionManager))),
                 new SimpleQuestionnaireQuestionService(new QuestionnaireQuestionDAO(jdbcConnectionManager)),
                 new LearnAlgorithmService(new LearnAlgorithmDAO(jdbcConnectionManager)),
-                new LearnAlgorithmController(), new AlertController());
+                new LearnAlgorithmController(),
+                new AlertController()));
+
+            this.LerntiaMainController(new LerntiaMainController(new MainLerntiaService(new SimpleLearningQuestionnaireService(new LearningQuestionnaireDAO(new QuestionnaireDAO(jdbcConnectionManager),jdbcConnectionManager)),
+                new SimpleQuestionService(new QuestionDAO(jdbcConnectionManager,new LearnAlgorithmDAO(jdbcConnectionManager))),
+                new SimpleQuestionnaireQuestionService(new QuestionnaireQuestionDAO(jdbcConnectionManager)),
+                new LearnAlgorithmService(new LearnAlgorithmDAO(jdbcConnectionManager)),
+                new LearnAlgorithmController(),
+                new AlertController()),
+
+                new AudioController(new SimpleTextToSpeechService(),new AlertController()),
+                new AlertController(),
+                new SimpleLearningQuestionnaireService(new LearningQuestionnaireDAO(new QuestionnaireDAO(jdbcConnectionManager),jdbcConnectionManager)),
+                new ZoomedImageController(new AlertController(),new WindowController(),new AudioController(new SimpleTextToSpeechService(),new AlertController())),
+                new SimpleExamResultsWriterService(new ExamResultsWriterDAO()),
+                new LearnAlgorithmController(),
+                new DirectoryChooserController(),
+                new SimpleUserService()));
 
         } catch (PersistenceException e) {
             LOG.error("Failed to get connection to test-database");
         }
     }
 
+    private void LerntiaMainController(LerntiaMainController lerntiaMainController) {
+        this.lerntiaMainController = lerntiaMainController;
+    }
+
+    private void IMainLerntiaService(MainLerntiaService mainLerntiaService) {
+        this.mainLerntiaService = mainLerntiaService;
+    }
+
     private void IQuestionnaireDAO(QuestionnaireDAO questionnaireDAO) {
         this.questionnaireDAO = questionnaireDAO;
     }
 
-    private void IMainLerntiaService(ILearningQuestionnaireService learningQuestionnaireService, IQuestionService questionService,
-                                     IQuestionnaireQuestionService questionnaireQuestionService, ILearnAlgorithmService learnAlgorithmService,
-                                     LearnAlgorithmController learnAlgorithmController, AlertController alertController) {
-        this.learningQuestionnaireService = learningQuestionnaireService;
-        this.questionService = questionService;
-        this.questionnaireQuestionService = questionnaireQuestionService;
-        this.learnAlgorithmService = learnAlgorithmService;
-        this.learnAlgorithmController = learnAlgorithmController;
-        this.alertController = alertController;
-    }
 
-    @Test // TODO: remove this test after testing the class MainLerntiaService
-    public void testSimpleLerntiaService() throws ServiceException {
-        Assert.assertThat(42, is(42));
-    }
 
 }
