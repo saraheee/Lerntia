79557b9cced7cedc38aa01786b67050834cd5399
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2020-05-09 05:18:43 +0200

    Start program in fullscreen

--- src/main/java/at/ac/tuwien/lerntia/application/MainApplication.java
+++ src/main/java/at/ac/tuwien/lerntia/application/MainApplication.java
@@ -45,6 +45,7 @@ public final class MainApplication extends Application {
         primaryStage.setTitle("[Lerntia] Lern- und Prüfungstool");
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/main.png")));
         primaryStage.setMaximized(true);
+        primaryStage.setFullScreen(true);
         primaryStage.centerOnScreen();
         primaryStage.setOnCloseRequest(event -> {
             alertController = new AlertController();
