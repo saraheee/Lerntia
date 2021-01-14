929f755f5a8183ab7f8251f8f3aa7d95756b9dd1
Sarah El-Sherbiny <sarah.el-sherbiny@tuwien.ac.at>
2021-01-14 05:45:22 +0100

    Implement sending the exam results per email #30

--- src/main/java/at/ac/tuwien/lerntia/lerntia/service/IExamResultsWriterService.java
+++ src/main/java/at/ac/tuwien/lerntia/lerntia/service/IExamResultsWriterService.java
@@ -8,9 +8,17 @@ public interface IExamResultsWriterService {
     /**
      * Write the exam results to a file for future reference
      *
-     * @param examwriter includes questions, name and path of exam
+     * @param examWriter includes questions, name and path of exam
      * @throws ServiceException if the exam results cannot be written
      */
-    void writeExamResults(ExamWriter examwriter) throws ServiceException;
+    void writeExamResults(ExamWriter examWriter) throws ServiceException;
+
+    /**
+     * Send the exam results per email to the user specified in the config file
+     *
+     * @param filePath the path of the exam results
+     * @throws ServiceException if the email cannot be send
+     */
+    void sendExamResultsPerEmail(String filePath) throws ServiceException;
 
 }
