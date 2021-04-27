979b6128ae103b7298ae2431233960f496380785
Philipp Rinner <raxyx@hotmail.com>
2021-04-27 14:50:50 +0200

    Answers in exam PDFs are now color-coded.

--- src/main/java/at/ac/tuwien/lerntia/lerntia/dao/IExamResultsWriterDAO.java
+++ src/main/java/at/ac/tuwien/lerntia/lerntia/dao/IExamResultsWriterDAO.java
@@ -3,6 +3,7 @@ package at.ac.tuwien.lerntia.lerntia.dao;
 import at.ac.tuwien.lerntia.exception.PersistenceException;
 import at.ac.tuwien.lerntia.lerntia.dto.ExamWriter;
 import at.ac.tuwien.lerntia.lerntia.dto.Question;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -55,7 +56,7 @@ public interface IExamResultsWriterDAO {
      * @return the cell with the image
      * @throws PersistenceException if the cell cannot be created
      */
-    PdfPCell getImageCellBox(URL path) throws PersistenceException;
+    PdfPCell getImageCellBox(URL path, BaseColor color) throws PersistenceException;
 
     /**
      * Get the image to a question
